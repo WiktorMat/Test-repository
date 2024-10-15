840040e279ad69f71678fafa08b76ea69b327c3f
Wiktor Radosław Matyszkiewicz <80032152+WiktorMat@users.noreply.github.com>
2024-10-15 14:31:27 +0200

    #IMC #3

--- fastMTT python port/SVfitIMC.cpp
+++ fastMTT python port/SVfitIMC.cpp
@@ -35,3 +35,169 @@
 
   integrand_ = g;
 }
+
+void SVfitIntegratorMarkovChain::integrate(gPtr_C g, const double* xl, const double* xu, unsigned d, double& integral, double& integralErr)
+{
+  setIntegrand(g, xl, xu, d);
+
+  if ( !integrand_ ) {
+    std::cerr << "<SVfitIntegratorMarkovChain>:"
+              << "No integrand function has been set yet --> ABORTING !!\n";
+    assert(0);
+  }
+
+  for ( unsigned iDimension = 0; iDimension < numDimensions_; ++iDimension ) {
+    xMin_[iDimension] = xl[iDimension];
+    xMax_[iDimension] = xu[iDimension];
+    if ( verbosity_ >= 1 ) {
+      std::cout << "dimension #" << iDimension << ": min = " << xMin_[iDimension] << ", max = " << xMax_[iDimension] << std::endl;
+    }
+  }
+
+//--- CV: set random number generator used to initialize starting-position
+//        for each integration, in order to make integration results independent of processing history
+  rnd_.SetSeed(12345);
+
+  numMoves_accepted_ = 0;
+  numMoves_rejected_ = 0;
+
+  unsigned k = numChains_*numBatches_;
+  unsigned m = numIterSampling_/numBatches_;
+
+  numChainsRun_ = 0;
+
+  if ( treeFileName_ != "" ) {
+    treeFile_ = new TFile(treeFileName_.data(), "RECREATE");
+    tree_ = new TTree("tree", "Markov Chain transitions");
+    for ( unsigned iDimension = 0; iDimension < numDimensions_; ++iDimension ) {
+      std::string branchName = Form("x%u", iDimension);
+      tree_->Branch(branchName.data(), &x_[iDimension]);
+    }
+    tree_->Branch("move", &treeMove_);
+    tree_->Branch("integrand", &treeIntegrand_);
+  }
+
+  for ( unsigned iChain = 0; iChain < numChains_; ++iChain ) {
+    bool isValidStartPos = false;
+    if ( initMode_ == kNone ) {
+      prob_ = evalProb(q_);
+      if ( prob_ > 0. ) {
+      bool isWithinBounds = true;
+      for ( unsigned iDimension = 0; iDimension < numDimensions_; ++iDimension ) {
+        double q_i = q_[iDimension];
+        if ( !(q_i > 0. && q_i < 1.) ) isWithinBounds = false;
+      }
+      if ( isWithinBounds ) {
+        isValidStartPos = true;
+      } else {
+        if ( verbosity_ >= 1 ) {
+          std::cerr << "<SVfitIntegratorMarkovChain>:"
+                    << "Warning: Requested start-position = " << format_vdouble(q_) << " not within interval ]0..1[ --> searching for valid alternative !!\n";
+        }
+      }
+      } else {
+        if ( verbosity_ >= 1 ) {
+          std::cerr << "<SVfitIntegratorMarkovChain>:"
+                    << "Warning: Requested start-position = " << format_vdouble(q_) << " returned probability zero --> searching for valid alternative !!";
+        }
+      }
+    }
+    unsigned iTry = 0;
+    while ( !isValidStartPos && iTry < maxCallsStartingPos_ ) {
+      initializeStartPosition_and_Momentum();
+      prob_ = evalProb(q_);
+      if ( prob_ > 0. ) {
+        isValidStartPos = true;
+      } else {
+        if ( iTry > 0 && (iTry % 100000) == 0 ) {
+          if ( iTry == 100000 ) std::cout << "<SVfitIntegratorMarkovChain::integrate>:" << std::endl;
+          std::cout << "try #" << iTry << ": did not find valid start-position yet." << std::endl;
+        }
+      }
+      ++iTry;
+    }
+    if ( !isValidStartPos ) continue;
+
+    for ( unsigned iMove = 0; iMove < numIterBurnin_; ++iMove ) {
+//--- propose Markov Chain transition to new, randomly chosen, point
+      bool isAccepted = false;
+      bool isValid = true;
+      do {
+  makeStochasticMove(iMove, isAccepted, isValid);
+      } while ( !isValid );
+    }
+
+    unsigned idxBatch = iChain*numBatches_;
+
+    for ( unsigned iMove = 0; iMove < numIterSampling_; ++iMove ) {
+//--- propose Markov Chain transition to new, randomly chosen, point;
+//    evaluate "call-back" functions at this point
+      bool isAccepted = false;
+      bool isValid = true;
+      do {
+        makeStochasticMove(numIterBurnin_ + iMove, isAccepted, isValid);
+      } while ( !isValid );
+      if ( isAccepted ) {
+        ++numMoves_accepted_;
+      } else {
+        ++numMoves_rejected_;
+      }
+
+      updateX(q_);
+      for ( std::vector<const ROOT::Math::Functor*>::const_iterator callBackFunction = callBackFunctions_.begin();
+            callBackFunction != callBackFunctions_.end(); ++callBackFunction ) {
+        (**callBackFunction)(x_);
+      }
+
+      if ( tree_ ) {
+        treeMove_ = iMove;
+        treeIntegrand_ = prob_;
+        tree_->Fill();
+      }
+
+      if ( iMove > 0 && (iMove % m) == 0 ) ++idxBatch;
+      assert(idxBatch < (numChains_*numBatches_));
+      probSum_[idxBatch] += prob_;
+    }
+
+    ++numChainsRun_;
+  }
+
+  for ( unsigned idxBatch = 0; idxBatch < probSum_.size(); ++idxBatch ) {
+    integral_[idxBatch] = probSum_[idxBatch]/m;
+    if ( verbosity_ >= 1 ) std::cout << "integral[" << idxBatch << "] = " << integral_[idxBatch] << std::endl;
+  }
+
+//--- compute integral value and uncertainty
+//   (eqs. (6.39) and (6.40) in [1])
+  integral = 0.;
+  for ( unsigned i = 0; i < k; ++i ) {
+    integral += integral_[i];
+  }
+  integral /= k;
+
+  integralErr = 0.;
+  for ( unsigned i = 0; i < k; ++i ) {
+    integralErr += square(integral_[i] - integral);
+  }
+  if ( k >= 2 ) integralErr /= (k*(k - 1));
+  integralErr = TMath::Sqrt(integralErr);
+
+  if ( verbosity_ >= 1 ) std::cout << "--> returning integral = " << integral << " +/- " << integralErr << std::endl;
+
+  errorFlag_ = ( numChainsRun_ >= 0.5*numChains_ ) ? 0 : 1;
+
+  ++numIntegrationCalls_;
+  numMovesTotal_accepted_ += numMoves_accepted_;
+  numMovesTotal_rejected_ += numMoves_rejected_;
+
+  if ( tree_ ) {
+    tree_->Write();
+  }
+  delete treeFile_;
+  treeFile_ = 0;
+  //delete tree_;
+  tree_ = 0;
+
+  if ( verbosity_ >= 1 ) print(std::cout);
+}
--- fastMTT python port/SVfitIntegratorMarkovChain.py
+++ fastMTT python port/SVfitIntegratorMarkovChain.py
@@ -54,26 +54,213 @@ class SVfitIntegratorMarkovChain:
             self.tree_file_.close()
             self.tree_file_ = None
     
-    def setIntegrand(self, g, xl, xu):
+    def setIntegrand(self, g, xl, xu):  ### <--- Tu wrócić, bo nie do końca rozumiem tę część kodu w cpp, ale to chyba tylko jakaś inicjalizacja
         # Przypisanie wartości do xMin_ i xMax_
-        self.xMin_ = xl
-        self.xMax_ = xu
+        self.xMin = xl
+        self.xMax = xu
 
         # Zmiana rozmiaru list epsilon0s_ i epsilon_
-        self.epsilon0s_ = [self.epsilon0_] * self.numDimensions_
-        self.epsilon_ = [0.0] * self.numDimensions_
+        self.epsilon0s = [self.epsilon0_] * self.numDimensions_
+        self.epsilon = [0.0] * self.numDimensions_
 
         # Zmiana rozmiaru list p_, q_, u_, pProposal_, qProposal_
-        self.p_ = [0.0] * (2 * self.numDimensions_)
-        self.q_ = [0.0] * self.numDimensions_
-        self.u_ = [0.0] * (2 * self.numDimensions_)
-        self.pProposal_ = [0.0] * self.numDimensions_
-        self.qProposal_ = [0.0] * self.numDimensions_
+        self.p = [0.0] * (2 * self.numDimensions_)
+        self.q = [0.0] * self.numDimensions_
+        self.u = [0.0] * (2 * self.numDimensions_)
+        self.pProposal = [0.0] * self.numDimensions_
+        self.qProposal = [0.0] * self.numDimensions_
 
         # Zmiana rozmiaru list probSum_ i integral_
-        self.probSum_ = [0.0] * (self.numChains_ * self.numBatches_)
-        self.integral_ = [0.0] * (self.numChains_ * self.numBatches_)
+        self.probSum = [0.0] * (self.numChains_ * self.numBatches_)
+        self.integral = [0.0] * (self.numChains_ * self.numBatches_)
 
         # Przypisanie wartości do integrand_
-        self.integrand_ = g
+        self.integrand = g
+
+    def registerCallBackFunction(self, function):
+        # Dodanie funkcji do listy callBackFunctions_
+        self.callBackFunctions_.append(function)
+
+    def evalProb(self, q):
+        return self.integrand(q, self.numDimensions, 0)
     
+    def initializeStartPosition_and_Momentum(self):
+        #Randomly choose start position of Markov Chain in N-dimensional space
+        for iDimension in range(self.numDimensions):
+            isInitialized = False
+            while not isInitialized:
+                q0 = 0
+                if self.init_mode == K_GAUS:
+                    q0 = np.random.normal(0.5, 0.5)
+                else:
+                    q0 = np.random.uniform(0, 1)
+                if q0 > 0 and q0 < 1:
+                    self.q[iDimension] = q0
+                    isInitialized = True
+        if self.verbosity >= 2:
+            print(f"<SVfitIntegratorMarkovChain::initializeStartPosition_and_Momentum>:")
+            print(f"q = {format_vdouble(self.q)}")
+    
+    def Integrate(self, g, xl, xu, d):
+        self.setIntegrand(g, xl, xu)
+
+        if self.integrand_ is None:
+            raise ValueError("<SVfitIntegratorMarkovChain>: No integrand function has been set yet --> ABORTING !!")
+        
+        for i in range(self.numDimensions_):
+            self.xMin[i] = xl[i]
+            self.xMax[i] = xu[i]
+            if self.verbosity >= 1:
+                print(f"dimension #{i}: min = {self.xMin[i]}, max = {self.xMax[i]}")
+
+        ## CV: set random number generator used to initialize starting-position
+        ## for each integration, in order to make integration results independent of processing history
+        np.random.seed(12345)
+
+        self.numMoves_accepted = 0
+        self.numMoves_rejected = 0
+
+        k = self.numChains * self.numBatches
+        m = self.numIterSampling / self.numBatches
+
+        numChainsrun = 0
+
+        #Tu odpowiednik "if ( treeFileName_ != "" ) {" (zapisywanie do pliku) z biblioteką h5py (napisane przez copilota)
+        #Dokładną implementację wymyślę później / naradzę się, czy korzystamy z h5py
+        #Można też korzystać z roota, ale pewnie trzeba zastanowić się co będzie najszybsze
+        """if self.treeFileName_ != "":
+            with h5py.File(self.treeFileName_, "w") as f:
+                tree_group = f.create_group("tree")
+                for iDimension in range(self.numDimensions_):
+                    branchName = f"x{iDimension}"
+                    tree_group.create_dataset(branchName, data=self.x_[iDimension])
+                tree_group.create_dataset("move", data=self.treeMove_)
+                tree_group.create_dataset("integrand", data=self.treeIntegrand_)
+        """
+
+
+        for iChain in range(self.numChains):
+            isValidStartPos = False
+            if self.init_mode == K_NONE:
+                self.prob = self.evalProb(q)
+                if self.prob > 0:
+                    isWithinBounds = True
+                    for iDimension in range(self.numDimensions):
+                        q_i = self.q[iDimension]
+                        if not (q_i > 0 and q_i < 1):
+                            isWithinBounds = False
+                        if isWithinBounds:
+                            isValidStartPos = True
+                        else:
+                            if self.verbosity >= 1:
+                                print(f"<SVfitIntegratorMarkovChain>: Warning: Requested start-position = {self.format_vdouble(self.q)} not within interval ]0..1[ --> searching for valid alternative !!")
+                else:
+                    print(f"<SVfitIntegratorMarkovChain>: Warning: Requested start-position = {self.format_vdouble(self.q)} returned probability zero --> searching for valid alternative !!")
+            iTry = 0
+            while (not isValidStartPos and iTry < self.maxCallsStartingPos):
+                self.initializeStartPosition_and_Momentum()
+                self.prob = self.evalProb(self.q)
+                if self.prob > 0:
+                    isValidStartPos = True
+                else:
+                    if (iTry > 0 and iTry % 100000 == 0):
+                        if iTry == 100000:
+                            print(f"<SVfitIntegratorMarkovChain>: Warning: Requested start-position = {self.format_vdouble(self.q)} returned probability zero --> searching for valid alternative !!")
+            if not isValidStartPos:
+                continue
+
+            for iMove in range(self.num_iter_burnin):
+                isAccepted = False
+                isValid = True
+                while not isValid:
+                    self.makeStochasticMove(iMove, isAccepted, isValid)
+
+            idxBatch = iChain*self.numBatches
+
+            for iMove in range(self.numIterSampling):
+                isAccepted = False
+                isValid = True
+                while not isValid:
+                    self.makeStochasticMove(iMove, isAccepted, isValid)
+
+            if isAccepted:
+                self.numMoves_accepted += 1
+            else:
+                self.numMoves_rejected += 1
+            
+            self.updateX(self.q)
+
+            for callBackFunction in self.callBackFunctions:
+                callBackFunction(self.x_)
+
+            #Z h5py:
+            """
+            if self.tree:
+                self.treeMove = iMove
+                self.treeIntegrand = self.prob
+                data = [self.treeMove, self.treeIntegrand]
+                with h5py.File('data.h5', 'a') as f:
+                    tree_group = f['tree']
+                    tree_group['dataset'].resize((tree_group['dataset'].shape[0] + 1), axis=0)
+                    tree_group['dataset'][-1] = data
+            """
+
+            if iMove > 0 and (iMove % m) == 0:
+                idxBatch += 1
+            assert idxBatch < (self.numChains * self.numBatches)
+            self.probSum[idxBatch] += self.prob
+    
+            self.ChainsRun += 1
+        
+        ### Jeszcze do przepisania:
+        
+        for ( unsigned idxBatch = 0; idxBatch < probSum_.size(); ++idxBatch ) {
+    integral_[idxBatch] = probSum_[idxBatch]/m;
+    if ( verbosity_ >= 1 ) std::cout << "integral[" << idxBatch << "] = " << integral_[idxBatch] << std::endl;
+  }
+
+//--- compute integral value and uncertainty
+//   (eqs. (6.39) and (6.40) in [1])
+  integral = 0.;
+  for ( unsigned i = 0; i < k; ++i ) {
+    integral += integral_[i];
+  }
+  integral /= k;
+
+  integralErr = 0.;
+  for ( unsigned i = 0; i < k; ++i ) {
+    integralErr += square(integral_[i] - integral);
+  }
+  if ( k >= 2 ) integralErr /= (k*(k - 1));
+  integralErr = TMath::Sqrt(integralErr);
+
+  if ( verbosity_ >= 1 ) std::cout << "--> returning integral = " << integral << " +/- " << integralErr << std::endl;
+
+  errorFlag_ = ( numChainsRun_ >= 0.5*numChains_ ) ? 0 : 1;
+
+  ++numIntegrationCalls_;
+  numMovesTotal_accepted_ += numMoves_accepted_;
+  numMovesTotal_rejected_ += numMoves_rejected_;
+
+  if ( tree_ ) {
+    tree_->Write();
+  }
+  delete treeFile_;
+  treeFile_ = 0;
+  //delete tree_;
+  tree_ = 0;
+
+  if ( verbosity_ >= 1 ) print(std::cout);
+}
+
+
+        
+        ###
+
+
+
+
+        #Placeholder dla właściwego kodu
+        integral = 0.0
+        integralErr = 0.0
+        return integral, integralErr
